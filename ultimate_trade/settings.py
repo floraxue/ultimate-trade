0583d7eb2ae8826ff7d4ea90e9676a1620bb2d1e
Flora Xue <xuezhenruo@berkeley.edu>
2016-11-12 14:38:01 -0800

    update project settings

--- ultimate_trade/settings.py
+++ ultimate_trade/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'ultimate_trade.trader',
 )
 
 MIDDLEWARE_CLASSES = (
--- ultimate_trade/trader/models.py
+++ ultimate_trade/trader/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     # Add link to default User model
     user = models.OneToOneField(User)
-    fb_name = models.CharField()
-    phone_number = models.CharField()
+    fb_name = models.TextField()
+    phone_number = models.TextField()
 
 
 class Category(models.Model):
-	name = models.CharField()
+	name = models.TextField()
 	parent_category = models.ForeignKey("self", 
 									    related_name="subcategory",
                                			on_delete=models.CASCADE, 
@@ -29,11 +29,11 @@ class SaleItem(models.Model):
 	sold_status = models.CharField(max_length=2, 
 								   choices=SOLD_STATUS_CHOICES)
 	created_on = models.DateTimeField()
-	primary_image = models.ImageField()
-	secondary_image = models.ImageField()
-	optional_image = models.ImageField(null=True)
+	primary_image = models.ImageField(upload_to='images/user_upload/')
+	secondary_image = models.ImageField(upload_to='images/user_upload/')
+	optional_image = models.ImageField(upload_to='images/user_upload/', null=True)
 	description = models.TextField()
-	price = models.IntegerField()
+	price = models.DecimalField(max_digits=11, decimal_places=2)
 	category = models.ForeignKey(Category, related_name="all_items",
 								 on_delete=models.CASCADE)
 
