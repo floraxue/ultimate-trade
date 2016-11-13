db826d7ccb6f62e23d6bdae8e80d02fa23a1bb65
Flora Xue <xuezhenruo@berkeley.edu>
2016-11-12 16:51:40 -0800

    make database

--- ultimate_trade/settings.py
+++ ultimate_trade/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'ultimate_trade.trader',
+    'trader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'ultimate_trade.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'trader',
+        'USER': 'tr',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
--- ultimate_trade/urls.py
+++ ultimate_trade/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^auth/', include('rest_framework.urls',
+                           namespace='rest_framework')),
 ]
